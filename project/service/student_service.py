62ec93ee289af513159dc4cbbdeeba14aec31e24
SantiagoLocatelli <slocatelli@fi.uba.ar>
2023-08-14 21:45:36 -0300

    retry

--- project/service/student_service.py
+++ project/service/student_service.py
@@ -17,22 +17,29 @@ def get_student_by_dni(dni):
         return OK, student.to_dict(), HTTPStatus.OK
 
 def register_student(dni, discord_id):
-    with BaseSQLConnection("pensamiento_computacional") as base_dao:
-        student_dao = StudentDao(base_dao.get_session())
-        student = student_dao.get_student_by_dni(dni)
+    cantidad = 0
+    cantidad_maxima = 5
+    while cantidad < cantidad_maxima:
+        try:
+            with BaseSQLConnection("pensamiento_computacional") as base_dao:
+                student_dao = StudentDao(base_dao.get_session())
+                student = student_dao.get_student_by_dni(dni)
 
-        if not student:
-            return STUDENT_NOT_FOUND, {}, HTTPStatus.NOT_FOUND
+                if not student:
+                    return STUDENT_NOT_FOUND, {}, HTTPStatus.NOT_FOUND
 
-        if student.registrado:
-            return STUDENT_ALREADY_REGISTERED, {}, HTTPStatus.BAD_REQUEST
-        
-        student.registrado = True
-        student.discord_id = discord_id
-        student_dao.session.commit()
+                if student.registrado:
+                    return STUDENT_ALREADY_REGISTERED, {}, HTTPStatus.BAD_REQUEST
+                
+                student.registrado = True
+                student.discord_id = discord_id
+                # student_dao.session.commit()
 
-        return OK, student.to_dict(), HTTPStatus.OK
-    
+                return OK, student.to_dict(), HTTPStatus.OK
+        except:
+            cantidad += 1
+
+
 def get_students_by_states(states):
     with BaseSQLConnection("pensamiento_computacional") as base_dao:
         student_dao = StudentDao(base_dao.get_session())
