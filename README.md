69ba7c57330ca2c42cf1d8dc7fdcca8fce6f4083
SantiagoLocatelli <slocatelli@fi.uba.ar>
2024-03-19 00:01:38 -0300

    delete unused files

--- README.md
+++ README.md
@@ -88,3 +88,6 @@ insert into alumno (cuit, nombre, cuatrimestre_id, registrado, estado, discord_i
 
 
 export FLASK_APP=project/__init__.py &&  python3 manage.py run
+
+##### obtener alumnos repetidos
+select cuit from alumno group by cuit having count(*) = 2;
--- project/db/inserts.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO cuatrimestre (description) values ('2023 1C');
-INSERT INTO cuatrimestre (description) values ('2023 2C');
-
-INSERT INTO alumno (cuit, nombre, cuatrimestre_id, estado)
-values ('41672583', 'Santiago Locatelli', 1, 1);
-INSERT INTO alumno (cuit, nombre, cuatrimestre_id, estado)
-values ('41672584', 'Carlos Locatelli', 1, 1);
